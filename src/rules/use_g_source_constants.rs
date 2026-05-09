6798cba051590edb88c988b45d4c81394c6cdd4d
Benjamin Otte <otte@gnome.org>
2026-05-09 21:33:50 +0200

    use_g_source_constants: Add gtk_widget_add_tick_callback

--- src/rules/use_g_source_constants.rs
+++ src/rules/use_g_source_constants.rs
@@ -44,6 +44,7 @@ impl Rule for UseGSourceConstants {
                     "g_timeout_add_seconds",
                     "g_timeout_add_full",
                     "g_timeout_add_seconds_full",
+                    "gtk_widget_add_tick_callback",
                 ]) {
                     if let Some(name) = self.extract_callback_name(call, &file.source) {
                         callbacks.insert(name);
@@ -77,7 +78,7 @@ impl UseGSourceConstants {
         let func_name = call.function_name_str()?;
         let callback_arg_index: usize = match func_name {
             "g_idle_add" => 0,
-            "g_idle_add_full" | "g_timeout_add" | "g_timeout_add_seconds" => 1,
+            "g_idle_add_full" | "g_timeout_add" | "g_timeout_add_seconds" | "gtk_widget_add_tick_callback" => 1,
             "g_timeout_add_full" | "g_timeout_add_seconds_full" => 2,
             _ => return None,
         };
